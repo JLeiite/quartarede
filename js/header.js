b994676923fc3fa9a49708a9623e715f3ab213a1
Ju Leiite <105261049+JLeiite@users.noreply.github.com>
2023-06-18 20:24:25 -0300

    Contatos e erros adicionados

--- js/header.js
+++ js/header.js
@@ -16,12 +16,18 @@ function toggleAnimation() {
 // Customizando os links da página
 let navLink = document.querySelector('.nav');
 let link = ["TRABALHOS", "SOBRE NÓS", "CONTATO"];
-let url = ["projetos.html", "index.html#sobreNos", "index.html#contato"]
+let url = ["projetos.html", "index.html#sobreNos", "index.html#contato"];
+let navDrop = document.querySelector('.dropdown-menu');
 
 for (let i = 0; i < link.length; i++) {
 
-  let listItemElement = document.createElement('li'); // Cria um novo elemento de item de lista
-  listItemElement.innerHTML = `<li class="nav-item"><a class="nav-link text-dark" aria-current="page" href="${url[i]}">${link[i]}</a></li>`;
-
+  let listItemElement = document.createElement('div'); // Cria um novo elemento de item de lista
+  listItemElement.innerHTML = `<li class="nav-item d-none d-sm-block"><a class="nav-link text-dark" aria-current="page" href="${url[i]}">${link[i]}</a></li>`;
   navLink.appendChild(listItemElement); // Adiciona o elemento de item de lista ao elemento de lista principal
+  
+  // Dropdown
+  let navDropdown = document.createElement('li'); 
+  navDropdown.innerHTML = `<a class="dropdown-item" href="${url[i]}">${link[i]}</a>`;
+
+  navDrop.appendChild(navDropdown);
 }
--- js/pilares.js
+++ js/pilares.js
@@ -9,7 +9,7 @@ for (let i = 0; i < link.length; i++) {
   let pilaresElement = document.createElement('div'); // Cria um novo elemento de item de lista
   pilaresElement.classList.add('col-md-4', 'col-sm-12');
 
-  pilaresElement.innerHTML = `<img src="${pilar[i]}" alt="${nomePilar[i]}">`;
+  pilaresElement.innerHTML = `<img src="${pilar[i]}" alt="${nomePilar[i]}" class="mb-3">`;
 
   pilares.appendChild(pilaresElement); // Adiciona o elemento de item de lista ao elemento de lista principal
 
@@ -19,7 +19,7 @@ for (let i = 0; i < link.length; i++) {
     pilaresElement.style.opacity = 0; // Define a opacidade para 0
 
     setTimeout(function() {
-        pilaresElement.innerHTML = `<div class="p-4 text-light" style="background-color: #203554">
+        pilaresElement.innerHTML = `<div class="p-4 text-light mb-3" style="background-color: #203554">
         <h5>${nomePilar[i]}</h5>
         <p>${pilarHover[i]}</p>
         </div>`;
@@ -29,8 +29,27 @@ for (let i = 0; i < link.length; i++) {
   });
   
   pilaresElement.addEventListener('mouseout', function() {
-    pilaresElement.innerHTML = `<img src="${pilar[i]}" alt="${nomePilar[i]}">`;
+    pilaresElement.innerHTML = `<img src="${pilar[i]}" alt="${nomePilar[i]}" class="mb-3">`;
   });
 
+}
+
+// Customizando contatos
+let contatosLink = document.querySelector('.contatosLink');
+let contatosDados = ['Whatsapp', 'Email', 'Instagram'];
+let contatoImg = ['img/whatsapp.png', 'img/email.png', 'img/instagram.png'];
+let contatosURL = ['https://wa.me/5511940038574', 'mailto:contato@quartarede.com.br', 'https://www.instagram.com/quartarede/'];
+
+for(let i = 0; i < contatosDados.length; i++){
+  let contatosElement = document.createElement('div'); // Cria um novo elemento de contato
+  contatosElement.classList.add('col-sm-12', 'col-md-4', 'mb-3');
+  contatosElement.innerHTML = `<div class="card">
+    <div class="card-body">
+      <img src="${contatoImg[i]}" alt="${contatosDados[i]}" class="contatoImg mb-4">
+      <a href="${contatosURL[i]}" class="btn btn-outline-secondary d-block">Fale conosco</a>
+    </div>
+  </div>`;
+
+  contatosLink.appendChild(contatosElement);
 }
 
